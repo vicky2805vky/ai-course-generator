1c8ffb9e8f4d5245f5defcafceed03bb30afa2e4
vicky2805vky <vicky2805vky@gmail.com>
2025-04-01 23:13:43 +0530

    chore: set up Drizzle ORM and create course table on Neon PostgreSQL

---
+++ src/schema/courseSchema.ts
@@ -0,0 +1,15 @@
+import { image } from "@/components/searchImage/types";
+import { chapterStateType } from "@/types/chapterSliceTypes";
+import { json, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const courseTable = pgTable("course", {
+  id: varchar("id").primaryKey().notNull(),
+  ownerId: varchar("ownerId").notNull(),
+  courseTitle: varchar("courseTitle").notNull(),
+  courseDescription: varchar("courseDescription").notNull(),
+  category: varchar("category").notNull(),
+  level: varchar("level").notNull(),
+  totalDuration: varchar("totalDuration").notNull(),
+  chapters: json().$type<chapterStateType>().notNull(),
+  courseImage: json().$type<image>(),
+});
